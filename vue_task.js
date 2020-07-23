caf8b9829bd624419da6151bd62eeb2f7e7e859b
ofion <ofion@bk.ru>
2020-07-23 19:53:59 +0300

    mes

--- vue_task.js
+++ vue_task.js
@@ -1,7 +1,5 @@
 Vue.directive('mask', VueMask.VueMaskDirective);
 
-
-
 var app = new Vue({
     el: '#app',
     components: {
@@ -9,60 +7,55 @@ var app = new Vue({
     },
     data: {
         urls: {
-            departmentsUrl: 'http://localhost:8080/alice/testserver',
-            serverURL: 'www.google.com',
+
+            serverURL: 'http://localhost:8080/alice/testserver',
         },
-        current: '',
+        isPreloaderActive: false,
+        isTable: false,
+        currentDepartment: '',
         timeMask: [ /[0-2]/, /[0-9]/, ':',/[0-5]/,/[0-9]/, ' - ', /[0-2]/, /[0-9]/, ':',/[0-5]/,/[0-9]/],
         departmentsList: [],
-        isPreloaderActive: false,
-        groupsList :[{
-            type: 'Вечерняя',
-            startDate: new Date(2016, 9,  16),
-            daysOfTheWeek:['Понедельник','Суббота','Среда',],
-            status:'Донабор',
-            timeLessons: '12:40-17:25',
-        },
+        groupsList :[],
 
-            {
-            type: 'Вечерняя',
-            startDate: new Date(2012, 3,  11),
-            daysOfTheWeek:['Понедельник','Среда',],
-            status:'Донабор',
-            timeLessons: '10:40-14:00',
-        },
-            {
-                type: 'Ночная',
-                startDate: new Date(2018, 2,  5),
-                daysOfTheWeek:['Среда'],
-                status:'Идет набор',
-                timeLessons: '13:40-19:30',
-            }],
     },
     created: function () {
         this.isPreloaderActive = true;
-        axios.get(this.urls.departmentsUrl)
-            .then((response) => {
-                this.departments = response.data;
-            })
-            .catch((error) => {
-                console.error(error + " --- error in get departments!")
-            })
+        axios({
+            method: 'get',
+            url: this.urls.serverURL,
+            params: {
+                department: "all",
+            }
+        }).then((response) => {
+            this.departmentsList = response.data;
+        }).catch((error) => {
+            console.error(error + " --- error in get departments!")
+        });
+
         this.isPreloaderActive = false;
     },
     methods: {
         getTable() {
             this.isPreloaderActive = true;
+            this.isTable=true;
             axios({
                 method: 'get',
                 url: this.urls.serverURL,
                 params: {
-                    department: this.current,
+                    department: this.currentDepartment,
                 }
+            }).then((response) => {
+                this.groupsList=response.data;
             }).catch((error) => {
                 console.error(error + " --- error in get table")
             });
             this.isPreloaderActive = false;
+        },
+        deleteGroup(index){
+            this.groupsList.splice(index, 1);
+        },
+        addNewGroup(){
+
         }
     }
 });
